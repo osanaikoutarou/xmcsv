7576b0500aa91e91034a285e4225d9eafea9def6
osanai <osanai@cr-040.local>
2017-11-09 14:03:32 +0900

    Initial Commit

---
+++ xmcsv/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  xmcsv
+//
+//  Created by osanai on 2017/11/09.
+//  Copyright © 2017年 osanai. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ xmcsv/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  xmcsv
+//
+//  Created by osanai on 2017/11/09.
+//  Copyright © 2017年 osanai. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
